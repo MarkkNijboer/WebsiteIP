7c030b775d33c8f469bd3f9ef4a1b01889322c9b
Mark Nijboer <marknijboer8@gmail.com>
2017-03-15 15:51:02 +0100

    + Restyled the extension

--- ip.js
+++ ip.js
@@ -1,26 +1,38 @@
 // Set variables
 var websiteIP_status, setPosition;
 var url = window.location.host;
+var knownHosts = {
+	"94.212.38.9" : "home",
+	"141.138.197.91" : "srv01.puuridee.nl",
+	"185.21.191.92" : "srv02.puuridee.nl",
+	"79.170.95.158" : "srv03.puuridee.nl",
+	"85.222.229.90" : "srv04.puuridee.nl"
+};
 
 $(document).ready(function() {
-	
+
 	// Set position to left for these websites
 	var noRight = new Array();
 		noRight[0] = "www.facebook.com";
 		noRight[1] = "www.google.com";
-		
+
 	//Check if on noRight array and set position accordingly
 	var noRightCheck = $.inArray(url, noRight);
-	
+
 	if (noRightCheck >= 0) {
 		setPosition = "left";
 	}
 	else {
 		setPosition = "right";
 	}
-	
+
 	chrome.extension.sendMessage({name: "getIP"}, function(response) {
 		var finalIP = response.domainToIP;
+
+		if (typeof knownHosts[finalIP] !== "undefined")
+		{
+			finalIP = knownHosts[finalIP] + " - " + finalIP;
+		}
 		chrome.extension.sendMessage({name: "getOptions"}, function(response) {
 			var websiteIP_status = response.enableDisableIP;
 			if (websiteIP_status == "Disable" || typeof websiteIP_status == 'undefined') {
@@ -28,7 +40,7 @@ $(document).ready(function() {
 			}
 		});
 	});
-	
+
 	$("#chrome_websiteIP").live('mouseover', function() {
 		if ($(this).hasClass('chrome_websiteIP_right')) {
 			$(this).removeClass("chrome_websiteIP_right");
@@ -39,7 +51,7 @@ $(document).ready(function() {
 			$(this).addClass("chrome_websiteIP_right");
 		}
 	});
-	
+
 	loadOptions(); //To set default value on pop-up button
 
 });
@@ -47,7 +59,7 @@ $(document).ready(function() {
 function loadOptions() {
 	chrome.extension.sendMessage({name: "getOptions"}, function(response) {
 		var enableDisableIP = response.enableDisableIP;
-		
+
 		// set default as disabled
 		if (typeof enableDisableIP == 'undefined') {
 			chrome.extension.sendMessage({name: "setOptions", status: 'Disable'}, function(response) {});
@@ -60,12 +72,12 @@ document.addEventListener('DOMContentLoaded', function () {
 	chrome.extension.sendMessage({name: "getOptions"}, function(response) {
 		$("#EnableDisableIP").val(response.enableDisableIP);
 	});
-	
+
 	document.querySelector('input').addEventListener('click', function() {
 		if ($('#EnableDisableIP').val() == "Disable") {
 			// save to localstore
 			chrome.extension.sendMessage({name: "setOptions", status: 'Enable'}, function(response) {});
-			$('#EnableDisableIP').val('Enable')	
+			$('#EnableDisableIP').val('Enable')
 		}
 		else if ($('#EnableDisableIP').val() == "Enable") {
 			// save to localstore
